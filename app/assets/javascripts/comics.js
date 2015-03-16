e5f1b889b65ac64869fbfcbaeab4c5f314caa00e
Ryan <ryanpauley@gmail.com>
2015-03-16 23:22:19 +1100

    cleaning up display

--- app/assets/javascripts/comics.js
+++ app/assets/javascripts/comics.js
@@ -1,18 +1,17 @@
 $(function(){
+	$('.throbber').hide();
 	$('#top').on('click', function(){
 		window.location = window.location.origin;
 	});
 	
 	$('#searchbutton').on('click', function(e){
+		$('.throbber').show();
 		e.preventDefault();
 		var ts = new Date().getMilliseconds();
 		var url = window.location.origin;
 		var search = $('#search').val();
 		var apiLib = $('#apiLibrary').val();
 		window.location = url + '/comics/?search=' + search + '&lib=' + apiLib + "&ts=" + ts
-		// window.location.replace(url + "/comics/?search=" + search + "&lib=" + apiLib);
-		$('#container').append('<p><a href="' + url + '/comics/?search=' + search + '&lib=' + apiLib + '">' + 
-						url + '/comics/?search=' + search + '&lib=' + apiLib + "&ts=" + ts + '</a></p>');
 	})
 
 	$('li').on('click', 'a', function(e){
