2979838da968b859b59e16b0d00218e89e854e52
Ryan <ryanpauley@gmail.com>
2015-03-16 19:14:04 +1100

    character and comics working :

--- app/assets/javascripts/comics.js
+++ app/assets/javascripts/comics.js
@@ -1,12 +1,23 @@
 $(function(){
+	$('#top').on('click', function(){
+		window.location = window.location.origin;
+	});
+	
 	$('#searchbutton').on('click', function(e){
 		e.preventDefault();
+		var ts = new Date().getMilliseconds();
 		var url = window.location.origin;
 		var search = $('#search').val();
 		var apiLib = $('#apiLibrary').val();
-		window.location = url + '/comics/?search=' + search + '&lib=' + apiLib
+		window.location = url + '/comics/?search=' + search + '&lib=' + apiLib + "&ts=" + ts
 		// window.location.replace(url + "/comics/?search=" + search + "&lib=" + apiLib);
 		$('#container').append('<p><a href="' + url + '/comics/?search=' + search + '&lib=' + apiLib + '">' + 
-						url + '/comics/?search=' + search + '&lib=' + apiLib + '</a></p>');
+						url + '/comics/?search=' + search + '&lib=' + apiLib + "&ts=" + ts + '</a></p>');
+	})
+
+	$('li').on('click', 'a', function(e){
+		e.preventDefault();
+		var ts = new Date().getMilliseconds();
+		window.location = window.location + "&nts=" + ts + "&links=" + $(this).attr('value');
 	})
 })
--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -3,31 +3,43 @@
 class ComicsController < ApplicationController
 		
 	def index
-		if params[:lib] || params[:search]
-			timestamp = Time.now.seconds_since_midnight()
-			ts = timestamp.to_s
-			apikey= "d8c9ccab08d4170328675a86bf56d5b8"
-			baseurl = "http://gateway.marvel.com/v1/public/"
-			key = ENV['MARVEL_API_KEY']
+		@apikey= "d8c9ccab08d4170328675a86bf56d5b8"
+		baseurl = "http://gateway.marvel.com/v1/public/"
+		@key = ENV['MARVEL_API_KEY']
 
-			hash = Digest::MD5.hexdigest(ts + key + apikey)
+		if params[:lib] || params[:search]
+			ts = params[:ts]
+
+			hash = Digest::MD5.hexdigest(ts + @key + @apikey)
 			
 			
 			url = baseurl + params[:lib] + 
 				"?ts=" + ts +
-				"&apikey=" + apikey +
+				"&apikey=" + @apikey +
 				"&hash=" + hash
 				if params[:search]
+					#add bit to handle two-name names
 					url = url + "&name=" + params[:search]
 				end
 
 			@parsedurl = url
-
-			
-
 			response = HTTParty.get(url)
 			@response = JSON.parse(response.body)
 		end
+		
+		if params[:links]
+			baseurl = params[:links]
+			ts = params[:nts]
+			hash = Digest::MD5.hexdigest(ts + @key + @apikey)
+			url = baseurl +
+			"?ts=" + ts +
+			"&apikey=" + @apikey +
+			"&hash=" + hash
+			@linkurl = url
+			response = HTTParty.get(url)
+			@link = JSON.parse(response.body)
+		end
+
 	end
 
 end
